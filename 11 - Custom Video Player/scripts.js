f51d1cc2ad0cd0df8ce85c6cafc9f36b6553045b
Selçuk Şaşoğlu <selcuk.sasoglu@gmail.com>
2018-09-23 19:13:21 +0200

    Added basic video control functions.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,2 +1,26 @@
-// custom video player here
+function skip(e) {
+    video.currentTime +=  parseFloat(this.dataset.skip);
+}
+
+function playPause(e) {
+    video.paused ? video.play() : video.pause();
+}
+
+function updatePlayPauseButton(e) {
+    const icon = this.paused ? '►' : '❚❚';
+    playPauseButton.textContent = icon;
+}
+
+const video = document.querySelector('video');
+const playPauseButton = document.querySelector('.toggle');
 const player = document.querySelector('.player');
+const skipButtons = document.querySelectorAll('[data-skip]');
+
+document.addEventListener('keypress', (e) => { if(e.code === 'Space') { playPause(e); }});
+video.addEventListener('keypress', (e) => { if(e.code === 'Space') { playPause(e); }});
+video.addEventListener('click', playPause);
+video.addEventListener('pause', updatePlayPauseButton);
+video.addEventListener('play', updatePlayPauseButton);
+playPauseButton.addEventListener('click', playPause);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
